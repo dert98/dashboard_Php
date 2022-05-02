76b65e74d008c437b61dc02094e05192e20f1a5e
Oscar <43421688+dert98@users.noreply.github.com>
2022-05-02 20:37:27 -0300

    add all
    new web implement dashboard

---
+++ web1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>web1</title>
+</head>
+<body>
+	<div class="table_data">
+		<table>
+		<tbody><tr><th colspan="2"><b>Information</b></th></tr>
+		<tr><td><b>Standard Specification Compliant</b></td><td>DOCSIS 3.0</td></tr>
+		<tr><td><b>Hardware Version</b></td><td>V2.0</td></tr>
+		<tr><td><b>Software Version</b></td><td>TLC_30.2.42</td></tr>
+		<tr><td><b>Model Name</b></td><td>FAST3686</td></tr>
+		<tr><td><b>Linux S/W Version</b></td><td>2.6.30-1.7.1mp2-svn6</td></tr>
+		<tr><td><b>Cable Modem MAC Address</b></td><td>24:7f:20:07:69:c0</td></tr>
+		<tr><td><b>Cable Modem Serial Number</b></td><td>DM1625504002009</td></tr>
+		<tr style="display:none;"><td><b>CM certificate</b></td><td>Installed</td></tr>
+		</tbody></table>
+		<table>
+		<tbody><tr><th colspan="2"><b>Status</b></th></tr>
+		<tr><td><b>System Up Time</b></td><td>5 days 09h:20m:24s</td></tr>
+		<tr><td><b>Network Access</b></td><td>Allowed</td></tr>
+		<tr></tr>
+		</tbody></table>
+	</div>
+</body>
+</html>
---
+++ web2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>web1</title>
+</head>
+<body>
+	<div class="text-center">
+		<img src="wifi.PNG">
+	</div>
+</body>
+</html>
